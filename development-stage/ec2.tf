a2fd5fd6b8bb0bb90203a8770f50d03ca8558c68
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-03 12:44:46 +0530

    CIDR given 0.0.0.0

--- development-stage/ec2.tf
+++ development-stage/ec2.tf
@@ -14,14 +14,14 @@ resource "aws_security_group" "dev-stage-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.dev-stage-sg.id
-#   cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_ipv4" {
   security_group_id = aws_security_group.dev-stage-sg.id
-#   cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -29,7 +29,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_8080_ipv4" {
   security_group_id = aws_security_group.dev-stage-sg.id
-#   cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8080
   ip_protocol       = "tcp"
   to_port           = 8080
