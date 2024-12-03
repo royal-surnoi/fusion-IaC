579b014058d2d49c468c9a221bfcae847fbaf4b0
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-03 12:12:18 +0530

    tags added

--- development-stage/ec2.tf
+++ development-stage/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "development-instance" {
   ami = var.AMI_id
-  instance_type =  "t2.micro"
+  instance_type =  var.instance_type
   tags = var.tags
 }
--- development-stage/variable.tf
+++ development-stage/variable.tf
@@ -3,12 +3,20 @@ variable "AMI_id"{
     default = "ami-0453ec754f44f9a4a"
 }
 
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
 variable "tags" {
     type = map
     default = {
-        Name = "dev-deploy"
-        Project = "fusion-iq"
-        Envronment = "dev"
+        Name = "DevelopmentServer"
+        Application = "fusion-iq"
+        Envronment  = "Development"
+        Owner       = "DevOpsTeam"
+        CostCenter  = "DevOpsTeam"
+        Component   = "frontend-backend"
     }
   
 }
