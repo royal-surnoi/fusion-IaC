422b28918221560abc8e1d030158240d50a31a11
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-03 14:44:19 +0530

    loop

--- development-stage/ec2.tf
+++ development-stage/ec2.tf
@@ -29,11 +29,10 @@ resource "aws_security_group" "dev-stage-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_8080_ipv4" {
   security_group_id = aws_security_group.dev-stage-sg.id
-  description       = [for description in var.ingress_rule : var.ingress_rule.description ]
-  cidr_ipv4         = [for cidr_ipv4 in var.ingress_rule : var.ingress_rule.cidr_blocks ]
-  from_port         = [for from_port in var.ingress_rule : var.ingress_rule.from_port ]
-  ip_protocol       = [for ip_protocol in var.ingress_rule : var.ingress_rule.ip_protocol ]
-  to_port           = [for to_port in var.ingress_rule : var.ingress_rule.to_port ]
+  description       = [for description in var.ingress_rule.description ]
+  cidr_ipv4         = [for cidr_ipv4 in var.ingress_rule.from_port ]
+  ip_protocol       = [for ip_protocol in var.ingress_rule.ip_protocol ]
+  to_port           = [for to_port in var.ingress_rule.to_port ]
 }
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.dev-stage-sg.id
