33f52f87ded24b8bb339be0f90c6b7102049cba4
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-03 12:43:12 +0530

    dev-stage-sg reourse name

--- development-stage/ec2.tf
+++ development-stage/ec2.tf
@@ -13,14 +13,14 @@ resource "aws_security_group" "dev-stage-sg" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.dev-stage-sg.id
 #   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.dev-stage-sg.id
 #   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 80
   ip_protocol       = "tcp"
@@ -28,14 +28,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_ipv4" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_8080_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.dev-stage-sg.id
 #   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 8080
   ip_protocol       = "tcp"
   to_port           = 8080
 }
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.dev-stage-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
