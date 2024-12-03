c08e347150e4ea2a30b9f716a110a96361a724b8
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-03 12:48:42 +0530

    allow 22 port remove https port

--- development-stage/ec2.tf
+++ development-stage/ec2.tf
@@ -15,9 +15,9 @@ resource "aws_security_group" "dev-stage-sg" {
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.dev-stage-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
+  from_port         = 22
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 22
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_ipv4" {
   security_group_id = aws_security_group.dev-stage-sg.id
