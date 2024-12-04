f9c4e1395e22a67accd5f5fe0c2b8c6d0f8bdf8e
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-04 10:41:47 +0530

    dev

--- development-stage/ec2.tf
+++ development-stage/ec2.tf
@@ -53,7 +53,7 @@ resource "aws_route53_zone" "dev" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = aws_route53_zone.dev.zone_id
   name    = "dev.royalreddy.co.in"
   type    = "A"
   ttl     = 300
